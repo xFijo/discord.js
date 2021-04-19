a818129152615714e42ad475263bec4138d7a456
xFijo <75216577+xFijo@users.noreply.github.com>
2021-04-19 21:29:54 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+const discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+
+const client = new Client({
+  disableMentions: 'everyone'
+});
+
+client.on('ready', () => {
+  console.log('[SERVER]', `Client ready as ${client.user.tag}.`);
+  client.user.setActivity('*help', { type: 'WATCHING' })
+});
+
+
+client.on('message', async message => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+});
+
+
+if (message.content === 'ping') { // regular commands
+   message.channel.send('pong!')
+};
+
+const Embed = new MessageEmbed()
+.setColor('RANDOM')
+.setTitle('title')
+.setDescription('description')
+.setFooter('footer')
+//.setTimestamp()     "optional"
+
+if (message.content === 'embed') {
+  message.channel.send(Embed);
+};
+
+console.log('congrats on your bot lmao');
+
+client.login(config.token);
